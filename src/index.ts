4e6099ec975fbf153522ac9c84de295dbf80a7bd
邓强锡 <dengqiangxi@askingdata.com>
2018-12-04 17:01:31 +0800

    去掉Google分析

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,3 @@ if (!('customElements' in self)) {
 } else {
   init();
 }
-
-if (typeof PRERENDER === 'undefined') {
-  window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
-  ga('create', 'UA-128752250-1', 'auto');
-  ga('set', 'transport', 'beacon');
-  ga('send', 'pageview');
-    // Load the GA script
-  const s = document.createElement('script');
-  s.src = 'https://www.google-analytics.com/analytics.js';
-  document.head!.appendChild(s);
-}
